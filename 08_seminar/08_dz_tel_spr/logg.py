5c375d115998edb6d5f742bcb8c5359a241eb692
Admin <admin@chermet.net>
2022-08-19 11:24:51 +0300

    tel book

--- 08_seminar/08_dz_tel_spr/logg.py
+++ 08_seminar/08_dz_tel_spr/logg.py
@@ -3,14 +3,14 @@ from time import time
 from pathlib import Path
 from creater_file import newcontact
 
-def data_logger(firstname, lastname, phoneNum, emailID):
+def data_logger(contactDetails):
     path_loger = Path('08_seminar', '08_dz_tel_spr', 'logger-tel.csv')
     time = datetime.now().strftime('%H:%M')
     with open(path_loger, 'a') as log_file:
        #log_file.write(time + ';' + data + ';\n')   
         log_file.write('\n {}\n'.format(time))
-        log_file.write('\n {} {}  {} \n {}\n'
-                .format(firstname, lastname, phoneNum, emailID))
+        log_file.write('\n imya{} familia{} nomer tel {} \n e-mail{}\n'
+                .format(contactDetails))
 
 # def data_logger(x, y, z, l):
 #     path_loger = Path("08_seminar", "logger.csv")
@@ -22,14 +22,3 @@ def data_logger(firstname, lastname, phoneNum, emailID):
         
 #         log_file.write('\nЧисло А: {} {} Число В: {} \nResult: {}\n'
 #                         .format(x, l, y, z))
-
-    # firstname = input_firstname() 
-    # lastname = input_lastname() 
-    # phoneNum = input("Введите номер телефона: ") 
-    # emailID = input("Введите ваш E-mail: ") 
-    # contactDetails =(f"" + firstname + " " + lastname + "  " + phoneNum + "  " + emailID +  "\n") 
-    # # myfile = open(filename, "a") 
-    # # myfile.write(contactDetails) 
-    # with open('Tel_book.txt', 'a') as myfile:
-    #     myfile.write(contactDetails)
-    # print("Новая запись добавлена в телефонный справочник: \n " + contactDetails + "")
